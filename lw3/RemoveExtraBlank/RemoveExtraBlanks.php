2982ba29c71e2893779efbe3d61153a68a32f48e
Daniel <getup.one@bk.ru>
2021-06-02 18:35:45 +0300

    Исправлен код

--- lw3/RemoveExtraBlank/RemoveExtraBlanks.php
+++ lw3/RemoveExtraBlank/RemoveExtraBlanks.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
     $text = isset($_GET['text']) ? $_GET['text'] : null;
-    echo $text . '<br>';
+    
     if ($text !== null)
     {
         $currChar = '';
         $oldChar = '';
         $printedChar = '';
-
+        
         for ($i = 0; $i < strlen($text); $i++)
-        {
+        { 
             $oldChar = $currChar;
             $currChar = $text[$i];
-            
+
             if ($currChar !== ' ')
             {
                 $printedChar = $currChar;
                 echo $printedChar;
             }
-
-            if ($currChar === '' && $oldChar !== '' && $printedChar !== '')
+            
+            if ($currChar === ' ' and $oldChar !== ' ' and $printedChar !== ' ')
             {
-                $printedChar = '';
+                $printedChar = ' ';
                 echo $printedChar;
             }
         }
-    }    
+    }   
